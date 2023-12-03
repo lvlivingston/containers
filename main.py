6a136a8edf7f119890e909889c51238d88f48344
lvlivingston <lvlivingston@gmail.com>
2023-12-03 16:45:50 -0600

    finished exercise

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # Python Containers
 # Lists, Tuples & Dictionaries
 
-student = {
-    'name': 'Maria',
-    'course': 'SEI',
-    'current_week': 7,
-}
+# student = {
+#     'name': 'Maria',
+#     'course': 'SEI',
+#     'current_week': 7,
+# }
 
-name = student['name']
+# name = student['name']
 # print(name)
-student['name'] = 'Tina'
+# student['name'] = 'Tina'
 
 # # print(student.get('skills', 'No Skills'))
 # print(student.get('skills', {
@@ -17,8 +17,8 @@ student['name'] = 'Tina'
 #     'JAVASCRIPT': 4
 # }))
 
-student['age'] = 21
-print(student)
+# student['age'] = 21
+# print(student)
 
 # del student['course']
 
@@ -43,10 +43,62 @@ print(student)
 # for key, value in student.items():
 #     print( f"key = {key} / value = {value}" )
 
-where_my_things_are = {
-    'car': 'in the garage',
-    'cell phone': 'by my side at all times'
-}
+# where_my_things_are = {
+#     'car': 'in the garage',
+#     'phone': 'in my pocket',
+#     'money': 'in the bank'
+# }
 
-for thing, location in where_my_things_are.items():
-    print( f" My {thing} is kept {location}")
+# for thing, location in where_my_things_are.items():
+#     print( f" My {thing} is kept {location}")
+
+#  LISTS (Like JavaScript arrays)
+# colors = ['red', 'green', 'blue']
+# colors[-1] = 'brown'
+# colors.append('purple')
+# colors.extend(['orange', 'black'])
+# colors += ['white', 'yellow']
+# colors.insert(1, 'pink')
+# colors.pop(2)
+# colors.remove('orange')
+# print(colors)
+
+# odds = [1, 3, 5]
+# evens = [2, 4, 6]
+# nums = odds + evens
+# print(nums)
+
+# colors = ['red', 'green', 'blue']
+# for color in colors:
+#   print(color)
+
+# what if you need the index value? this is not best practice
+# idx = 0
+# for color in colors:
+#   print( idx, color )
+#   idx += 1
+
+# for item in enumerate(colors):
+#     print( item )
+
+# This is considered best practice if you need access to the index of the iteration
+# for idx, color in enumerate(colors):
+#     print( idx, color )
+
+scores = [
+    {
+        # 'name': 'name of the player',
+        'name': 'Kaitlin',
+        'points': 25  # points the player scored
+    }
+]
+
+scores.append({
+    'name': 'Jim', 
+    'points': '15'
+    })
+
+print(scores)
+
+for score in scores:
+    print( f"{score['name']} scored {score['points']} points." )
